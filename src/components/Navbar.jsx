15c03d9d552b80bd2b179343ffd6258f938456d3
urajesh-hub <rurmails@gmail.com>
2024-11-20 20:05:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,56 +4,34 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav
-      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      className="navbar navbar-dark bg-dark"
       style={{ height: "40px", paddingTop: "5px", paddingBottom: "5px" }}
     >
-      <div className="container">
-        {/* Logo link */}
-        <Link to="/" className="navbar-brand">
+      <div className="container d-flex justify-content-around align-items-center">
+        {/* Logo */}
+        <Link to="/" className="navbar-brand d-flex align-items-center">
           <img
             src="/images/sjjmlogo.jpg"
             alt="Logo"
-            className="navbar-logo"
-            style={{ height: "30px", width: "auto" }}
+            style={{ height: "30px", width: "auto", marginRight: "5px" }}
           />
+          <span style={{ color: "white", fontSize: "18px" }}>Company Name</span>
         </Link>
-        {/* Toggle button for mobile screens */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
         {/* Navbar links */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to="/emp-list" className="nav-link text-white">
-                Employee Master
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/meal-form" className="nav-link text-white">
-                Meals Entry Form
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Entry-meals" className="nav-link text-white">
-                Meals Data
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/calculated-meals" className="nav-link text-white">
-                Reports
-              </Link>
-            </li>
-          </ul>
+        <div className="d-flex justify-content-around" style={{ width: "100%" }}>
+          <Link to="/emp-list" className="nav-link text-white">
+            Employee Master
+          </Link>
+          <Link to="/meal-form" className="nav-link text-white">
+            Meals Entry Form
+          </Link>
+          <Link to="/Entry-meals" className="nav-link text-white">
+            Meals Data
+          </Link>
+          <Link to="/calculated-meals" className="nav-link text-white">
+            Reports
+          </Link>
         </div>
       </div>
     </nav>
